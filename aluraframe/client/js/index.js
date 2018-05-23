077fa6ff11a43db38b0a7f44213206ac964298e0
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-05-23 17:46:59 -0300

    js to insert data

---
+++ aluraframe/client/js/index.js
@@ -0,0 +1,29 @@
+var fields = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor'),
+];
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var tr = document.createElement('tr');
+
+    fields.forEach(function (field) {
+        var td = document.createElement('td');
+        td.textContent = field.value;
+        tr.appendChild(td);
+    });
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = fields[1].value * fields[2].value;
+
+    tr.appendChild(tdVolume);
+    tbody.appendChild(tr);
+
+    fields[0].value = '';
+    fields[1].value = '';
+    fields[2].value = '';
+
+    fields[0].focus();
+});
