9ad5c0c21d88277ac831e32a5e112529aed64c8d
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-05-30 10:57:04 -0300

    Clean form after submit

--- aluraframe/client/js/app/controllers/NegotiationController.js
+++ aluraframe/client/js/app/controllers/NegotiationController.js
@@ -15,10 +15,20 @@ class NegotiationController {
 
         let negotiation = this._createnegotiation();
         this._negotiations.add(negotiation);
+
+        this._cleanForm();
     }
 
     _createnegotiation() {
         let date = DateHelper.stringToDate(this._inputDate.value);
         return new Negotiation(date, this._inputQtd, this._inputValue);
+    }
+
+    _cleanForm () {
+        this._inputDate.value = '';
+        this._inputQtd.value = 1;
+        this._inputValue.value = '';
+
+        this._inputDate.focus();
     }
 }
